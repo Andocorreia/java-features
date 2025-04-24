2e51cb0a08ec73d08f083e007ef176120f87abbe
[CI&T] André Correia <andreoc@ciandt.com>
2025-04-24 09:05:20 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/treinamentos/diversos/JavaTreinamentosDiversosApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamentos.diversos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTreinamentosDiversosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTreinamentosDiversosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinamentos/diversos/JavaTreinamentosDiversosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamentos.diversos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTreinamentosDiversosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
