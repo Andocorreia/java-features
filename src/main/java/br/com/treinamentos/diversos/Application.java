26f578230259e68ecbcfe6cf89cd7502d6eae3fc
[CI&T] André Correia <andreoc@ciandt.com>
2025-04-25 15:50:47 -0300

    Cenarios de FactoryMethodCollection (Java 9)

--- src/main/java/br/com/treinamentos/diversos/Application.java
+++ src/main/java/br/com/treinamentos/diversos/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaTreinamentosDiversosApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaTreinamentosDiversosApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
