dff51bb7a5b8bd2b31bc4d0f9f465340846b305c
[CI&T] André Correia <andreoc@ciandt.com>
2025-05-10 14:08:29 -0300

    Functional Interface (Java 8)

---
+++ src/main/java/br/com/treinamentos/diversos/releases/java8/FunctionalInterfaces.java
@@ -0,0 +1,27 @@
+package br.com.treinamentos.diversos.releases.java8;
+
+//Explanation
+//A functional interface in Java is an interface that contains only one
+//abstract method. Functional interfaces can have multiple default or static
+//methods, but only one abstract method. Runnable, ActionListener, and
+//Comparator are common examples of Java functional interfaces.
+
+@FunctionalInterface
+interface Calculate {
+    abstract int calc(int a, int b);
+}
+
+class FunctionalInterfaces {
+
+    public void createdNewTread() {
+        //to implement Runnable
+        new Thread(() -> System.out.println("New Thread")).start();
+    }
+
+    public int sumValues(int a, int b) {
+        Calculate calculate = (x, y) -> x * y;
+        return calculate.calc(a, b);
+    }
+
+}
+
---
+++ src/test/java/br/com/treinamentos/diversos/releases/java8/FunctionalInterfacesTest.java
@@ -0,0 +1,27 @@
+package br.com.treinamentos.diversos.releases.java8;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FunctionalInterfacesTest {
+
+    @Test
+    void createdNewTread() {
+        FunctionalInterfaces functionalInterfaces = new FunctionalInterfaces();
+
+        functionalInterfaces.createdNewTread();
+
+
+        System.setOut(System.out); // Reset System.out
+    }
+
+    @Test
+    void sumValuesReturnsCorrectSumForPositiveNumbers() {
+        FunctionalInterfaces functionalInterfaces = new FunctionalInterfaces();
+        assertEquals(7, functionalInterfaces.sumValues(3, 4));
+    }
+}
