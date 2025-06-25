9a7dd38622ddbf3e47060a56094a8caf077c7c76
André Correia <aocorreia1@gmail.com>
2025-06-25 20:22:25 -0300

    Comparable vs Comparator (Java 8)

---
+++ src/test/java/br/com/treinamentos/diversos/releases/java8/ComparableComparatorTest.java
@@ -0,0 +1,31 @@
+package br.com.treinamentos.diversos.releases.java8;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ComparableComparatorTest {
+
+    @Test
+    void comparableShouldReturnFirstMovieAfterSortingByYear() {
+        ComparableComparator comparableComparator = new ComparableComparator();
+        Movie result = comparableComparator.comparable();
+
+        assertNotNull(result);
+        assertEquals("Star Wars", result.getName());
+        assertEquals(1977, result.getYear());
+    }
+
+    @Test
+    void comparatorShouldReturnFirstMovieAfterSortingByName() {
+        ComparableComparator comparableComparator = new ComparableComparator();
+        Movie result = comparableComparator.Comparator();
+
+        assertNotNull(result);
+        assertEquals("Empire Strikes Back", result.getName());
+        assertEquals(1980, result.getYear());
+    }
+}
