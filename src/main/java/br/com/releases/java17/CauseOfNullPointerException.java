bef290ca26c67d07e22be033fceca702749323b4
André Correia <aocorreia1@gmail.com>
2025-06-27 22:55:45 -0300

    Cause of NullPointerException (Java 17)

--- src/main/java/br/com/releases/java17/CauseOfNullPointerException.java
+++ src/main/java/br/com/releases/java17/CauseOfNullPointerException.java
@@ -1,5 +1,8 @@
 package br.com.releases.java17;
 
+//Explanation
+// This code demonstrates a common cause of a NullPointerException in Java.
+// It occurs when trying to access a method or property of an object that is null.
 public class CauseOfNullPointerException {
 
         public void name() {
